b714c3cde98fd3276c0f26ced71378e871aacee5
Undebrethale <mcdonald12kfc30@gmail.com>
2024-10-11 08:07:36 -0700

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,22 @@
+
+let currentIndex = 0;
+
+const images = ["xpadsix.jpg", "packageimg.jpg", "fanitemimg.jpg"];
+const imageElement = document.getElementById("image");
+
+function switchImage(){
+
+    currentIndex++
+
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+
+    imageElement.src = images[currentIndex];
+
+}
+
+setInterval(switchImage, 4333);
+
+
+// Why can you use javascript with out an JDK but need one for actual Java? //
